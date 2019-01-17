682dc9871b2e8fe327e9e5a6e87b4f2b6e29384a
flynn <lognamu@gmail.com>
2019-01-17 15:19:23 +0900

    all js file move into question directory

---
+++ questions/filtering_modulable.js
@@ -0,0 +1,9 @@
+function solution(arr, divisor) {
+    var answer = [];
+    answer = arr.filter(v => v % divisor == 0).sort((a,b) => a - b);
+    return answer;
+}
+
+console.log(
+    solution([2, 36, 1, 3], 1)
+);
